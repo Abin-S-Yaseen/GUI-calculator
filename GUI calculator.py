4d43ec93ab292de6e3d092763b3b5d25156d595e
Abin-S-Yaseen <abinchingeli12@gmail.com>
2021-06-03 01:02:33 +0530

    Update GUI calculator.py

--- GUI calculator.py
+++ GUI calculator.py
@@ -10,3 +10,13 @@ def button_click(number):
     current = check_box.get()
     check_box.delete(0,END)
     check_box.insert(0,str(current) + str(number))
+
+def button_add():
+    first_number = check_box.get()
+    if len(check_box.get()) == 0:
+        first_number = 0
+    global f_num
+    global math
+    math = 'addition'
+    f_num = int(first_number)
+    check_box.delete(0,END)
